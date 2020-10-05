f96d0f29063cf21e75b1449fa86c4a78bdaec554
Ankit Ayush <ankitayush4250@gmail.com>
2020-10-06 01:16:19 +0530

    Updates

---
+++ precache-manifest.f9a074ff373f62972bf8cc3505a15d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9834c36f1a290efa38843baa567807a",
+    "url": "/Covid19-Stats-React.js-Project/index.html"
+  },
+  {
+    "revision": "b94abda8dfe6b0d574d9",
+    "url": "/Covid19-Stats-React.js-Project/static/css/main.db388a60.chunk.css"
+  },
+  {
+    "revision": "545ffe706e438fd96a31",
+    "url": "/Covid19-Stats-React.js-Project/static/js/2.f285ada2.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid19-Stats-React.js-Project/static/js/2.f285ada2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94abda8dfe6b0d574d9",
+    "url": "/Covid19-Stats-React.js-Project/static/js/main.a7e63aa3.chunk.js"
+  },
+  {
+    "revision": "e8836ea156483f67be02",
+    "url": "/Covid19-Stats-React.js-Project/static/js/runtime-main.78aa3b9d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-Stats-React.js-Project/static/media/image.d7265326.png"
+  }
+]);
